7151cb2b302d109cb60d85e8524873975cf3d09d
Ailing Li <alofe9859@gmail.com>
2022-07-20 06:58:39 +0000

    Posts Controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  
+  
+  # =============CREATE========================
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Post created successfully!"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  # ================READ========================
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
+  def show 
+  end
+
+  # ===============EDIT==========================
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else 
+      render :edit
+    end
+  end
+
+  # ================DELETE=========================
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def find_post
+    @post = Post.find params[:id]
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+
+end
